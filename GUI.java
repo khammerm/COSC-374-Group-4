abe8b525fdbae868d23bbf58a859f22470b8c2ae
khammerm <khammerm@emich.edu>
2022-10-20 17:24:37 -0400

    Added a few more metrics, tweaked swing GUI

--- GUI.java
+++ GUI.java
@@ -1,12 +1,3 @@
-/*  Class: COSC 374
- *  ---------------
- *  Group 4 Members:
- *  Kevin Hammermeister
- *  Tyler Begg
- *  Chad Hollens
- *  John Zielinski
- *  ---------------
- */
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -23,22 +14,36 @@ public class GUI implements ActionListener{
 
 
     public GUI() {
-
         // basic GUI layout
         // initialize our frame/panel
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
+
         JButton button = new JButton("Authenticate");
-        JTextField hr = new JTextField();
-
         
+        JTextField hr = new JTextField();
+        JTextField gps = new JTextField();
+        JTextField bp = new JTextField();
 
+        JLabel hrLabel = new JLabel("Heart Rate:");
+        JLabel gpsLabel = new JLabel("Latitude/Longitude:");
+        JLabel bpLabel = new JLabel("Blood Pressure (Ex: 120/80):");
 
         panel.setBorder(BorderFactory.createEmptyBorder(320, 220, 320, 220));
         panel.setLayout(new GridLayout(0, 1));
-        panel.add(button);
-        panel.add(hr);
+        //hrLabel.setBounds(250, 250, 20, 20)
+        panel.setBackground(Color.LIGHT_GRAY);
 
+        hrLabel.setHorizontalAlignment(0);
+
+        panel.add(hrLabel);
+        panel.add(hr);
+        panel.add(gpsLabel);
+        panel.add(gps);
+        panel.add(bpLabel);
+        panel.add(bp);
+        panel.add(button);
+        
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("374 Group 4 Program");
--- program.java
+++ program.java
@@ -5,7 +5,6 @@ public class program extends GUI{
     public static void main(String[] args)
     {
         new GUI();
-
     }
     
 }
