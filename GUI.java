66114e32873829d8dd99262044a820386b330004
khammerm <khammerm@emich.edu>
2022-11-10 15:13:38 -0500

    Updated layout, started adding functionality to text fields.

--- GUI.java
+++ GUI.java
@@ -4,14 +4,18 @@ import java.awt.event.*;
 
 public class GUI implements ActionListener{
 
-    int hr;
+    // bool arr for metrics, if x amt == true then authentic
+    protected boolean[] auth = new boolean[10];
 
+    protected int hr;
+    
     // int arr for BP, ex [120][80];
-    int[] bp = new int[2];
+    private int[] bp = new int[2];
 
     // float arr for latitude/longitude
-    float[] gps = new float[3];
+    private float[] gps = new float[3];
 
+    private String pw = "temp";
     // fingerprint, retina scan, touchpad etc.,
     // timer for keystrokes
     // stride length
@@ -22,45 +26,65 @@ public class GUI implements ActionListener{
         // basic GUI layout
         // initialize our frame/panel
         JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        
+        JPanel infoPanel = new JPanel();
+        JPanel authPanel = new JPanel();
+
         JButton button = new JButton("Authenticate");
         
-        JTextField hr = new JTextField();
-        JTextField gps = new JTextField();
-        JTextField bp = new JTextField();
-        JTextField pw = new JTextField();
+        JTextField hrField = new JTextField();
+        JTextField gpsField = new JTextField();
+        JTextField bpField = new JTextField();
+        JTextField pwField = new JTextField();
 
         JLabel hrLabel = new JLabel("Heart Rate:");
         JLabel gpsLabel = new JLabel("Latitude/Longitude:");
         JLabel bpLabel = new JLabel("Blood Pressure (Ex: 120/80):");
         JLabel pwLabel = new JLabel("Password:");
 
-        panel.setBorder(BorderFactory.createEmptyBorder(320, 220, 320, 220));
-        panel.setLayout(new GridLayout(0, 1));
+        infoPanel.setBorder(BorderFactory.createEmptyBorder(0, 150, 120, 150));
+        authPanel.setBorder(BorderFactory.createEmptyBorder(20, 150, 0, 150));
+
+        authPanel.setLayout(new GridLayout(0,1));
+        infoPanel.setLayout(new GridLayout(0, 1));
         //hrLabel.setBounds(250, 250, 20, 20)
-        panel.setBackground(Color.LIGHT_GRAY);
+        authPanel.setBackground(Color.CYAN);
+        infoPanel.setBackground(Color.LIGHT_GRAY);
 
-        hrLabel.setHorizontalAlignment(0);
+        //hrLabel.setHorizontalAlignment(0);
 
-        panel.add(hrLabel);
-        panel.add(hr);
-        panel.add(gpsLabel);
-        panel.add(gps);
-        panel.add(bpLabel);
-        panel.add(bp);
-        panel.add(pwLabel);
-        panel.add(pw);
-        panel.add(button);
+        button.addActionListener(new ActionListener()
+            {
+                // just started working on grabbing value from textFields.
+                public void actionPerformed(ActionEvent e) {
+                    hr = Integer.valueOf(hrField.getText());
+                    if(hr >= 140 || hr <= 90)
+                    {
+                        auth[0] = false;
+                    }
+                }
+            }
+        );
+
+        infoPanel.add(hrLabel);
+        infoPanel.add(hrField);
+        infoPanel.add(gpsLabel);
+        infoPanel.add(gpsField);
+        infoPanel.add(bpLabel);
+        infoPanel.add(bpField);
+        infoPanel.add(pwLabel);
+        infoPanel.add(pwField);
+        authPanel.add(button);
         
-        frame.add(panel, BorderLayout.CENTER);
+        frame.add(infoPanel, BorderLayout.NORTH);
+        frame.add(authPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("374 Group 4 Program");
         frame.pack();
         frame.setVisible(true);
     }
+    public void actionPerformed(ActionEvent e)
+    {
 
-    public void actionPerformed(ActionEvent e) {
-        
     }
+    
 }
--- program.java
+++ program.java
@@ -1,7 +1,6 @@
 
 public class program extends GUI{
-    // bool arr for metrics, if x amt == true then authentic
-    boolean[] auth = new boolean[10];
+    
     public static void main(String[] args)
     {
         new GUI();
